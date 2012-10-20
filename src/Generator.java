712a2351d7ced3d006ee66290d3e469d3806afb8
Tom Leaman <tom.leaman@googlemail.com>
2012-10-20 21:49:08 +0100

    Add some generator implementation

--- src/Generator.java
+++ src/Generator.java
@@ -1,10 +1,37 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 
 public class Generator {
 	
+	private Graph graph;
+	
 	public List<String> generateLadder(String start, int depth) {
-		return null;
+		graph = new Graph("dict" + start.length() + ".dat");
+		return findLadder(start, depth);
+	}
+	
+	private List<String> findLadder(String start, int depth) {
+		List<String> ladder = new LinkedList<String>();
+		if (depth <= 0)
+			return ladder;
+		
+		ladder.add(start);
+		if (ladder.size() == depth)
+			return ladder;
+		
+		// MOAR CODEZ HERE PLOX
+		
+		return ladder;
+	}
+	
+	public static void main(String[] args) {
+		Generator g = new Generator();
+		List<String> ladder = g.generateLadder("click", 100);
+		for (int i = 0; i < ladder.size(); i++) {
+			System.out.println((i + 1) + " - " + ladder.get(i));
+		}
 	}
 	
 }
