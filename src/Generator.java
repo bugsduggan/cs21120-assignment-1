3e4b9fc5925037f553dcb1793b21f61099b4e9ff
Tom Leaman <tom.leaman@googlemail.com>
2012-10-21 00:01:01 +0100

    Non error catching algorithm

--- src/Generator.java
+++ src/Generator.java
@@ -5,29 +5,32 @@ import java.util.List;
 public class Generator {
 	
 	private Graph graph;
+	private int depth;
 	
 	public List<String> generateLadder(String start, int depth) {
 		List<String> ladder = new LinkedList<String>();
-		if (depth == 0)
+		if (depth <= 0)
 			return ladder;
 		ladder.add(start);
 		graph = new Graph("dict" + start.length() + ".dat");
+		this.depth = depth;
 		return findLadder(ladder, depth - 1);
 	}
 	
-	private List<String> findLadder(List<String> ladder, int depth) {
-		if (depth == 0)
+	private List<String> findLadder(List<String> ladder, int toFind) {
+		if (toFind <= 0)
 			return ladder;
 		
 		String current = ladder.get(ladder.size() - 1);
 		List<String> search = graph.getAdjacentWords(current);
+		search.removeAll(ladder);
 		
 		for (String s : search) {
-			if (!ladder.contains(s)) {
-				ladder.add(s);
-				findLadder(ladder, depth - 1);
+			ladder.add(s);
+			findLadder(ladder, toFind - 1);
+			if (ladder.size() == depth)
 				break;
-			}
+			ladder.remove(ladder.size() - 1);
 		}
 		
 		return ladder;
@@ -35,7 +38,7 @@ public class Generator {
 	
 	public static void main(String[] args) {
 		Generator g = new Generator();
-		List<String> ladder = g.generateLadder("click", 100);
+		List<String> ladder = g.generateLadder("too", 500);
 		for (int i = 0; i < ladder.size(); i++) {
 			System.out.println((i + 1) + " - " + ladder.get(i));
 		}
