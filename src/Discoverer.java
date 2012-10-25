cf0d2e1f0cd866de15a4a59f46ea56588a125a89
Tom Leaman <tom@tomleaman.co.uk>
2012-10-25 12:16:28 +0100

    Add some comments

--- src/Discoverer.java
+++ src/Discoverer.java
@@ -12,83 +12,85 @@ public class Discoverer {
   private HashMap<String, Integer> costs;
   private HashMap<String, String> predecessors;
 
-	public List<String> discoverLadder(String start, String end) {
-		
-		List<String> ladder = new LinkedList<String>();
-		if (start.length() != end.length())
-			return ladder;
-		
-		graph = new WordGraph(start.length());
-		if (!graph.hasNode(start) || !graph.hasNode(end))
-			return ladder;
-		
-		// end of variable declarations and sanity-checks
+  public List<String> discoverLadder(String start, String end) {
+    
+    List<String> ladder = new LinkedList<String>();
+    if (start.length() != end.length())
+      return ladder;
+    
+    graph = new WordGraph(start.length());
+    if (!graph.hasNode(start) || !graph.hasNode(end))
+      return ladder;
+    
+    // Init some extra structures to track progress
+    unvisited = new LinkedList<String>();
+    costs = new HashMap<String, Integer>();
+    predecessors = new HashMap<String, String>();
+    costs.put(start, 0);
+    unvisited.add(start);
 
-		List<String> visited = new LinkedList<String>();
-		unvisited = new LinkedList<String>();
-		costs = new HashMap<String, Integer>();
-		predecessors = new HashMap<String, String>();
-		costs.put(start, 0);
-		unvisited.add(start);
-		while (!unvisited.isEmpty()) {
-			String current = getMinimum(unvisited);
-			visited.add(current);
-			unvisited.remove(current);
-			findMinimalDistance(current);
-		}
+    // While there are still paths to search...
+    while (!unvisited.isEmpty()) {
+      String current = getMinimum(unvisited);
+      unvisited.remove(current);
+      // Update the cost for the node being searched
+      findMinimalDistance(current);
+    }
 
-		String current = end;
-		if (predecessors.get(current) == null)
-		  return null;
-		ladder.add(current);
-		while (predecessors.get(current) != null) {
-			current = predecessors.get(current);
-			ladder.add(current);
-		}
-		
-		Collections.reverse(ladder);
-		return ladder;
-	}
+    String current = end;
+    if (predecessors.get(current) == null)
+      return ladder; // We're out of ideas
+    ladder.add(current);
+    // Now work backwards through the predecessors map
+    while (predecessors.get(current) != null) {
+      current = predecessors.get(current);
+      ladder.add(current);
+    }
 
-	private void findMinimalDistance(String word) {
-		List<String> next = graph.getConnected(word);
-		for (String n : next) {
-		  if (shortestPath(n) > shortestPath(word) + 1) {
-				costs.put(n, shortestPath(word) + 1);
-				predecessors.put(n, word);
-				unvisited.add(n);
-		  }
-		}
-	}
-
-	private String getMinimum(List<String> next) {
-		String min = null;
-		for (String s : next) {
-		  if (min == null) {
-		    min = s;
-		  } else {
-		    if (shortestPath(s) < shortestPath(min)) {
-				  min = s;
-				}
-		  }
-		}
-		return min;
+    // Since we traversed predecessors backwards, we need to reverse the ladder
+    Collections.reverse(ladder);
+    return ladder;
   }
 
-	private int shortestPath(String word) {
-		Integer d = costs.get(word);
-		if (d == null) {
-		  return Integer.MAX_VALUE;
-		} else {
-		  return d;
-		}
+  private void findMinimalDistance(String word) {
+    List<String> next = graph.getConnected(word);
+    for (String n : next) {
+      if (shortestPath(n) > shortestPath(word) + 1) {
+        costs.put(n, shortestPath(word) + 1);
+        predecessors.put(n, word);
+        unvisited.add(n);
+      }
+    }
   }
 
-	public static void main(String[] args) {
-		Discoverer d = new Discoverer();
-		List<String> ladder = d.discoverLadder("head", "foot");
-		for (String s : ladder)
-			System.out.println(s);
-	}
-	
+  private String getMinimum(List<String> next) {
+    String min = null;
+    for (String s : next) {
+      if (min == null) {
+        min = s;
+      } else {
+        if (shortestPath(s) < shortestPath(min)) {
+          min = s;
+        }
+      }
+    }
+    return min;
+  }
+
+  private int shortestPath(String word) {
+    Integer d = costs.get(word);
+    if (d == null) {
+      return Integer.MAX_VALUE;
+    } else {
+      return d;
+    }
+  }
+
+  public static void main(String[] args) {
+    Discoverer d = new Discoverer();
+    List<String> ladder = d.discoverLadder("click", "stabs");
+    for (String s : ladder)
+      System.out.println(s);
+  }
+
 }
