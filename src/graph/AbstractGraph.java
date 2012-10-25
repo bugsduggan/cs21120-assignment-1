24377b55e707fb167638a792aef32310c36a13eb
Tom Leaman <tom@tomleaman.co.uk>
2012-10-25 19:48:44 +0100

    Start tidying the graph package

--- src/graph/AbstractGraph.java
+++ src/graph/AbstractGraph.java
@@ -3,56 +3,73 @@ package graph;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public abstract class AbstractGraph<T> implements Graph<T> {
 
-	protected HashMap<T, List<T>> map;
-	
-	public AbstractGraph() {
-		map = new HashMap<T, List<T>>();
-	}
-	
-	@Override
-	public void addNode(T elem) {
-		map.put(elem, new LinkedList<T>());
-	}
+  protected Map<T, List<T>> map;
 
-	@Override
-	public Set<T> nodeSet() {
-		return map.keySet();
-	}
+  public AbstractGraph() {
+    map = new HashMap<T, List<T>>();
+  }
 
-	@Override
-	public boolean hasNode(T elem) {
-		return map.containsKey(elem);
-	}
+  @Override
+  public void addNode(T elem) {
+    map.put(elem, new LinkedList<T>());
+  }
 
-	@Override
-	public boolean isEdge(T from, T to) {
-		if (!map.containsKey(from) || !map.containsKey(to))
-			return false;
-		return map.get(from).contains(to);
-	}
+  @Override
+  public Set<T> nodeSet() {
+    return map.keySet();
+  }
 
-	@Override
-	public int size() {
-		return map.size();
-	}
+  @Override
+  public boolean hasNode(T elem) {
+    return map.containsKey(elem);
+  }
 
-	@Override
-	public boolean isEmpty() {
-		return map.isEmpty();
-	}
+  @Override
+  public boolean isEdge(T from, T to) {
+    if (!map.containsKey(from) || !map.containsKey(to))
+      return false;
+    return map.get(from).contains(to);
+  }
 
-	@Override
-	public void removeNode(T elem) {
-		map.remove(elem);
-	}
-	
-	@Override
-	public List<T> getConnected(T elem) {
-		return map.get(elem);
-	}
+  @Override
+  public int size() {
+    return map.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return map.isEmpty();
+  }
+
+  @Override
+  public void removeNode(T elem) {
+    map.remove(elem);
+  }
+
+  @Override
+  public List<T> getConnected(T elem) {
+    return map.get(elem);
+  }
+
+  @Override
+  public void removeEdge(T from, T to) {
+    if (!map.containsKey(from))
+      addNode(from);
+    if (!map.containsKey(to))
+      addNode(to);
+    map.get(from).remove(to);
+    map.get(to).remove(from);
+  }
+
+  @Override
+  public void addEdge(T from, T to) {
+    map.get(from).add(to);
+    map.get(to).add(from);
+  }
 
 }
--- src/graph/WordGraph.java
+++ src/graph/WordGraph.java
@@ -1,59 +1,61 @@
 package graph;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
 import java.util.LinkedList;
 import java.util.List;
 
-public class WordGraph extends UndirectedGraph<String> {
-	
-	public WordGraph(int numLetters) {
-		try {
-		    String fileName = "dict" + numLetters + ".dat";
-	    	List<String> wordList = getWordList(fileName);
-			
-			for (String word : wordList) {
-				addNode(word);
-				for (String comparison : nodeSet()) {
-					if (oneDifference(word, comparison))
-						addEdge(word, comparison);
-				}
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Cannot find dictionary for " + numLetters + " size words.");
-			System.out.println("Files should be named dictX.dat where X is the number of letters.");
-			System.exit(1);
-		}
-	}
-	
-	private List<String> getWordList(String fileName) throws FileNotFoundException {
-		List<String> wordList = new LinkedList<String>();
-		File f = new File(fileName);
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(f));
-			String word = in.readLine();
-			while(word != null) {
-				wordList.add(word);
-				word = in.readLine();
-			}
-			in.close();
-		} catch (FileNotFoundException e) {
-			throw e;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return wordList;
-	}
-	
-	private boolean oneDifference(String a, String b) {
-		int diffs = 0;
-		for (int i = 0; (diffs <= 1) && (i < a.length()); i++) {
-			if (a.charAt(i) != b.charAt(i))
-				diffs++;
-		}
-		return (diffs == 1);
-	}
-	
+public class WordGraph extends AbstractGraph<String> {
+
+  public WordGraph(int numLetters) {
+    try {
+        String fileName = "dict" + numLetters + ".dat";
+        List<String> wordList = getWordList(fileName);
+
+      for (String word : wordList) {
+        addNode(word);
+        for (String comparison : nodeSet()) {
+          if (oneDifference(word, comparison))
+            addEdge(word, comparison);
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("Cannot find dictionary for " + numLetters + " size words.");
+      System.out.println("Files should be named dictX.dat where X is the number of letters.");
+      System.exit(1);
+    }
+  }
+
+  private List<String> getWordList(String fileName) throws FileNotFoundException {
+    List<String> wordList = new LinkedList<String>();
+    File f = new File(fileName);
+    try {
+      BufferedReader in = new BufferedReader(new FileReader(f));
+      String word = in.readLine();
+      while(word != null) {
+        wordList.add(word);
+        word = in.readLine();
+      }
+      in.close();
+    } catch (FileNotFoundException e) {
+      throw e;
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return wordList;
+  }
+
+  private boolean oneDifference(String a, String b) {
+    int diffs = 0;
+    for (int i = 0; (diffs <= 1) && (i < a.length()); i++) {
+      if (a.charAt(i) != b.charAt(i))
+        diffs++;
+    }
+    return (diffs == 1);
+  }
+
 }
