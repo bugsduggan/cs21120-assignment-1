3bdd85ae9f6b87cf01500a3bba3bcd85493bf2cf
Tom Leaman <tom@tomleaman.co.uk>
2012-10-25 18:49:18 +0100

    Flesh out driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,8 @@
+
+import ladder.*;
+
+import java.util.Scanner;
+
 public class Driver {
 
   public static void main(String[] args) {
@@ -21,20 +26,67 @@ public class Driver {
 
   private static void startInteractive() {
     System.out.println("Interactive Mode");
+    System.out.println();
 
+    Scanner in = new Scanner(System.in);
+    String input = "FOOBAR";
+
+    while (!input.equalsIgnoreCase("q")) {
+    // if the user types 'q' or 'Q', we exit
+
+      // print the menu
+      System.out.println();
+      System.out.println("Please make a selection");
+      System.out.println("    g - Generate ladder");
+      System.out.println("    d - Discover ladder");
+      System.out.println("    q - Quit");
+      System.out.println();
+      System.out.print(">> ");
+
+      input = in.nextLine();
+      if ("g".equalsIgnoreCase(input)) {
+        // generate mode
+        System.out.println("Please enter a start word");
+        System.out.print(">> ");
+        String start = in.nextLine();
+        System.out.println("Please enter a search depth");
+        System.out.print(">> ");
+        int depth = in.nextInt();
+        generateLadder(start, depth);
+      } else if ("d".equalsIgnoreCase(input)) {
+        // discover mode
+        System.out.println("Please enter a start word");
+        System.out.print(">> ");
+        String start = in.nextLine();
+        System.out.println("Please enter a target word");
+        System.out.print(">> ");
+        String end = in.nextLine();
+        discoverLadder(start, end);
+      }
+    }
+
+    in.close();
     System.exit(0);
   }
 
   private static void generateLadder(String start, int depth) {
     System.out.println("Generating Ladder");
+    System.out.println();
 
-    System.exit(0);
+    Generator g = new Generator();
+    Iterable<String> ladder = g.generateLadder(start, depth);
+    for (String word : ladder)
+      System.out.println(word);
   }
 
   private static void discoverLadder(String start, String end) {
     System.out.println("Discovering Ladder");
+    System.out.println();
 
-    System.exit(0);
+    Discoverer d = new Discoverer();
+    Iterable<String> ladder = d.discoverLadder(start, end);
+    for (String word : ladder)
+      System.out.println(word);
   }
 
   private static void printUsage() {
