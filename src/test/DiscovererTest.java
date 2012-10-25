aa35b5df2fcdfbdc46bd9c8da137962f3c172963
Tom Leaman <tom@tomleaman.co.uk>
2012-10-25 21:57:25 +0100

    Add implementation for Discoverer tests

--- src/test/DiscovererTest.java
+++ src/test/DiscovererTest.java
@@ -1,7 +1,8 @@
 package test;
 
-import static org.junit.Assert.*;
+import ladder.Discoverer;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -11,9 +12,27 @@ import org.junit.Test;
  */
 public class DiscovererTest {
 
+  private Discoverer discoverer = new Discoverer();
+
+  /**
+   * Verifies that it fails sensibly
+   */
   @Test
-  public void testDiscoverLadder() {
-    fail("Not yet implemented");
+  public void testDiscoverLadderFails() {
+    if (Utils.size(discoverer.discoverLadder("head", "head")) != 0)
+      fail("Starting and ending on the same word should return empty");
+  }
+
+  /**
+   * Verifies that it succeeds sensibly
+   */
+  @Test
+  public void testDiscoverLadderSucceeds() {
+    int result = Utils.size(discoverer.discoverLadder("head", "foot"));
+    if (result == 0)
+      fail("There should be a ladder from head to foot");
+    if (result > 10)
+      fail("It should take less than 10 moves to get from head to foot");
   }
 
 }
